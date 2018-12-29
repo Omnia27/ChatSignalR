b78d7693b7bdcf503458e0202d14b5f66f61d62d
Omnia27 <omniaiti38@gmail.com>
2018-12-29 10:52:04 -0800

    first commit

---
+++ ChatApp/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace ChatApp
+{   //server
+    //send all clients any updates
+    //end point client
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void Sendmessage(string message)
+        {
+            Clients.All.receivemessage(message);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Chat-Signal-R
